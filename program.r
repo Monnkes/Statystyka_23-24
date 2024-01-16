1ddcf981d04a5a42d0090127f02a6c04dd48b839
Mateusz <zsuetam36@gmail.com>
2024-01-16 22:54:26 +0100

    dzialajacy (mam nadzieje) algorytm PRS, dla narazie paru funkcji 3 wymiarow z przykladu w tresci zadania

--- program.r
+++ program.r
@@ -1,42 +1,42 @@
-library(smoof)
-library(ecr)
+pure_random_search <- function(objective_function, domain, num_points) {
+  best_point <- numeric(length(domain))
+  best_value <- Inf
 
-random <- function(param_set, dimensions) {
-    res <- numeric(dimensions)
-    
-    for (i in 1:dimensions) {
-        cat(param_set[1, i], " ", param_set[2, i], "\n")
-        res[i] <- runif(1, min = as.numeric(param_set[1, i]), max = as.numeric(param_set[2, i]))
-    }
-    
-    return(res)
-}
-
-pure_random_search <- function(param_set, budget, dimensions) {
-  ackley <- makeAckleyFunction(dimensions = dimensions)
-  best_value <- Inf 
-  
-  for (i in 1:budget) {
-    solutions <- random(param_set, dimensions)
-    cat("1\n", solutions, "\n")
-    value <- ackley(solutions)
-    
+  for (i in 1:num_points) {
+    random_point <- runif(length(domain)/2, min = domain[, 1], max = domain[, 2])
+    value <- objective_function(random_point)
     if (value < best_value) {
       best_value <- value
+      best_point <- random_point
     }
   }
-  
-  return(best_value)
+
+  return(list(best_point = best_point, best_value = best_value))
 }
 
-main <- function() {
-  budget <- 10
-  dimensions <- 2
-  par.set <- makeParamSet(
-    makeNumericParam("x1", lower = -5, upper = 5),
-    makeNumericParam("x2", lower = -5, upper = 5)
-  )
-  cat(pure_random_search(par.set$par.set, budget, dimensions))
+main <- function(){
+  ackley_function <- function(x) smoof::makeAckleyFunction(dimensions = length(x))(x)
+  rastrigin_function <- function(x) smoof::makeRastriginFunction(dimensions = length(x))(x)
+  schwefel_function <- function(x) smoof::makeSchwefelFunction(dimensions = length(x))(x)
+  rosenbrock_function <- function(x) smoof::makeRosenbrockFunction(dimensions = length(x))(x)
+
+  domain <- matrix(c(0, -2, 100, 1, 2, 1000), ncol = 2)
+
+  ackley_result <- pure_random_search(ackley_function, domain, num_points = 1000)
+  cat("Ackley function results:", "\n")
+  print(ackley_result)
+
+  rastrigin_result <- pure_random_search(rastrigin_function, domain, num_points = 1000)
+  cat("Rastrigin function results:", "\n")
+  print(rastrigin_result)
+
+  schwefel_result <- pure_random_search(schwefel_function, domain, num_points = 1000)
+  cat("Schwefel function results:", "\n")
+  print(schwefel_result)
+
+  rosenbrock_result <- pure_random_search(rosenbrock_function, domain, num_points = 1000)
+  cat("Rosenbrock function results:", "\n")
+  print(rosenbrock_result)
 }
 
 main()
