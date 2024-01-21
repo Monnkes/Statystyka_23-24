c0acdda03440f12354fd67126d3863a646f93063
Mateusz <zsuetam@onet.pl>
2024-01-21 18:34:43 +0100

    kolejne poprawki

--- PRS-boxplot.r
+++ PRS-boxplot.r
@@ -1,9 +1,11 @@
 library(plyr)
 library(ggplot2)
+library(smoof)
 
 pure_random_search <- function(objective_function, domain, num_points, dimensions) {
   best_point <- numeric(length(domain))
   best_value <- Inf
+  
   
   for (i in 1:num_points) {
     random_point <- runif(dimensions, min = domain[, 1], max = domain[, 2])
@@ -18,34 +20,37 @@ pure_random_search <- function(objective_function, domain, num_points, dimension
   return(best_value)
 }
 
-main <- function(){
-  ackley_function <- function(x) smoof::makeAckleyFunction(dimensions = length(x))(x)
-  rastrigin_function <- function(x) smoof::makeRastriginFunction(dimensions = length(x))(x)
+main <- function() {
+  ackley_function <- function(x) makeAckleyFunction(dimensions = length(x))(x)
+  rastrigin_function <- function(x) makeRastriginFunction(dimensions = length(x))(x)
   
-  dimensions2 = 2
-  domain2 <- matrix(c(rep(0, dimensions2), rep(10, dimensions2)), ncol = 2)
+  dimensions <- c(2, 10, 20)
+
+  ackley_results <- rastrigin_results <- numeric(0)
   
-  dimensions10 = 10
-  domain10 <- matrix(c(rep(0, dimensions10), rep(10, dimensions10)), ncol = 2)
-  
-  dimensions20 = 20
-  domain20 <- matrix(c(rep(0, dimensions20), rep(10, dimensions20)), ncol = 2)
-  
-  ackley_results2 <- replicate(50, pure_random_search(ackley_function, domain2, num_points = 10, dimensions2))
-  rastring_results2 <- replicate(50, pure_random_search(rastrigin_function, domain2, num_points = 10, dimensions2))
-  ackley_results10 <- replicate(50, pure_random_search(ackley_function, domain10, num_points = 10, dimensions10))
-  rastring_results10 <- replicate(50, pure_random_search(rastrigin_function, domain10, num_points = 10, dimensions10))
-  ackley_results20 <- replicate(50, pure_random_search(ackley_function, domain20, num_points = 10, dimensions20))
-  rastring_results20 <- replicate(50, pure_random_search(rastrigin_function, domain20, num_points = 10, dimensions20))
-  
+  for (dim in dimensions) {
+    ackley <- makeAckleyFunction(dim)
+    lower_bounds <- getLowerBoxConstraints(ackley)
+    upper_bounds <- getUpperBoxConstraints(ackley)
+    domain <- matrix(c(lower_bounds, upper_bounds), ncol = 2)
+    
+    ackley_results <- c(ackley_results, replicate(50, pure_random_search(ackley_function, domain, num_points = 10, dim)))
+    
+    rastrigin <- makeRastriginFunction(dim)
+    lower_bounds <- getLowerBoxConstraints(rastrigin)
+    upper_bounds <- getUpperBoxConstraints(rastrigin)
+    domain <- matrix(c(lower_bounds, upper_bounds), ncol = 2)
+    rastrigin_results <- c(rastrigin_results, replicate(50, pure_random_search(rastrigin_function, domain, num_points = 10, dim)))
+  }
+
   data <- data.frame(
-    Dimension = rep(c(2, 10, 20), each = 50),
-    Function = rep(c("Ackley", "Rastrigin"), each = 150),
-    Value = c(ackley_results2, rastring_results2, ackley_results10, rastring_results10, ackley_results20, rastring_results20)
+    Dimension = rep(dimensions, each = 100),
+    Function = rep(rep(c("Ackley", "Rastrigin"), each = 50), times = 3),
+    Value = c(ackley_results, rastrigin_results)
   )
   
   ggplot(data, aes(x = as.factor(Dimension), y = Value, fill = Function)) +
-    geom_boxplot() +
+    geom_boxplot(position = position_dodge(width = 0.75), width = 0.7) +
     labs(title = "Pure Random Search Optimization",
          x = "Dimension",
          y = "Optimization Value") +
