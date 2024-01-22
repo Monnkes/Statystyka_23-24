d7752f705713da522b856c17bba96ea2d7c61577
Mateusz <zsuetam@onet.pl>
2024-01-22 17:02:13 +0100

    dupadupadaup

--- Comparison_v2.r
+++ Comparison_v2.r
@@ -23,7 +23,7 @@ multi_start <- function(objective_function, domain, num_points, dimensions) {
     }
   }
 
-  return(c(best_value, best_point, total_calls/num_points))
+  return(best_value)
 }
 
 
@@ -40,58 +40,90 @@ pure_random_search <- function(objective_function, domain, num_points, dimension
     }
   }
 
-  return(c(best_value, best_point))
+  return(best_value)
+}
+
+statistical_analysis <- function(results1, results2) {
+  t_test_result <- t.test(results1, results2)
+  confidence_interval <- t_test_result$conf.int
+  p_value <- t_test_result$p.value
+  
+  return(list(confidence_interval = confidence_interval, p_value = p_value))
 }
 
 main <- function() {
-
-  # Ogólnie to działa jednak dane są takie aby to się dało jakkolwiek policzyć trzeba zmienić sposób liczenia średniej, dodać wymiary, ustawić poprawne ilosći wywołań
-
+  
   ackley_function <- function(x) smoof::makeAckleyFunction(dimensions = length(x))(x)
   rastrigin_function <- function(x) smoof::makeRastriginFunction(dimensions = length(x))(x)
-
-  dimensions <- c(10)
-
-  ackley_results <- rastrigin_results <- numeric(0)
-
+  
+  dimensions <- c(2, 10, 20)
+  
+  MS_ackley_avg <- PRS_ackley_avg <- MS_rastrigin_avg <- PRS_rastrigin_avg <- numeric(0)
+  dimensions_vector <- c()
+  
   for (dim in dimensions) {
-    print(dim)
-
+    
     ackley <- makeAckleyFunction(dim)
-    lower_bounds <- getLowerBoxConstraints(ackley)
-    upper_bounds <- getUpperBoxConstraints(ackley)
-    domain <- matrix(c(lower_bounds, upper_bounds), ncol = 2)
-
-    MS_ackley_results <- replicate(5, multi_start(ackley_function, domain, num_points = 10, dim))
-    print(MS_ackley_results)
-    MS_ackley_avg_calls <- mean(MS_ackley_results[dim + 2, ])
-    print(MS_ackley_avg_calls)
-    PRS_ackley_results <- replicate(5, multi_start(ackley_function, domain, num_points = MS_ackley_avg_calls, dim))
-    print(PRS_ackley_results)
-
+    ackley_lower_bounds <- getLowerBoxConstraints(ackley)
+    ackley_upper_bounds <- getUpperBoxConstraints(ackley)
+    ackley_domain <- matrix(c(ackley_lower_bounds, ackley_upper_bounds), ncol = 2)
+    
+    MS_ackley_results <- replicate(5, multi_start(ackley_function, ackley_domain, num_points = 1, dim))
+    PRS_ackley_results <- replicate(5, pure_random_search(ackley_function, ackley_domain, num_points = 1, dim))
+    
     rastrigin <- makeRastriginFunction(dim)
-    lower_bounds <- getLowerBoxConstraints(rastrigin)
-    upper_bounds <- getUpperBoxConstraints(rastrigin)
-    domain <- matrix(c(lower_bounds, upper_bounds), ncol = 2)
-
-    MS_rastrigin_results <- replicate(5, multi_start(rastrigin_function, domain, num_points = 10, dim))
-    print(MS_rastrigin_results)
-    MS_rastrigin_avg_calls <- mean(MS_rastrigin_results[dim + 2, ])
-    print(MS_rastrigin_avg_calls)
-    PRS_rastrigin_results <- replicate(5, pure_random_search(rastrigin_function, domain, num_points = MS_rastrigin_avg_calls, dim))
-    print(PRS_rastrigin_results)
-
-    MS_ackley_avg <- mean(MS_ackley_results[1, ])
-    PRS_ackley_avg <- mean(PRS_ackley_results[1, ])
-    MS_rastrigin_avg <- mean(MS_rastrigin_results[1, ])
-    PRS_rastrigin_avg <- mean(PRS_rastrigin_results[1, ])
-
-    print(MS_ackley_avg)
-    print(PRS_ackley_avg)
-    print(MS_rastrigin_avg)
-    print(PRS_rastrigin_avg)
+    rastrigin_lower_bounds <- getLowerBoxConstraints(rastrigin)
+    rastrigin_upper_bounds <- getUpperBoxConstraints(rastrigin)
+    rastrigin_domain <- matrix(c(rastrigin_lower_bounds, rastrigin_upper_bounds), ncol = 2)
+    
+    MS_rastrigin_results <- replicate(5, multi_start(rastrigin_function, rastrigin_domain, num_points = 1, dim))
+    PRS_rastrigin_results <- replicate(5, pure_random_search(rastrigin_function, rastrigin_domain, num_points = 1, dim))
+    
+    MS_ackley_avg <- c(MS_ackley_avg, mean(MS_ackley_results))
+    MS_rastrigin_avg <- c(MS_rastrigin_avg, mean(MS_rastrigin_results))
+    PRS_ackley_avg <- c(PRS_ackley_avg, mean(PRS_ackley_results))
+    PRS_rastrigin_avg <- c(PRS_rastrigin_avg, mean(PRS_rastrigin_results))
+    
+    dimensions_vector <- c(dimensions_vector, rep(dim, times = 4))
+    
+    
+    ackley_analysis <- statistical_analysis(MS_ackley_results, PRS_ackley_results)
+    rastrigin_analysis <- statistical_analysis(MS_rastrigin_results, PRS_rastrigin_results)
+    
+    print(paste("Ackley (", dim, " dimensions):"))
+    print(paste("Confidence Interval: ", ackley_analysis$confidence_interval))
+    print(paste("P-value: ", ackley_analysis$p_value))
+    
+    print(paste("Rastrigin (", dim, " dimensions):"))
+    print(paste("Confidence Interval: ", rastrigin_analysis$confidence_interval))
+    print(paste("P-value: ", rastrigin_analysis$p_value))
+    
   }
+  
+  values <- c()
+   for (i in 1:length(dimensions)) {
+     values <- c(values, MS_ackley_avg[i], MS_rastrigin_avg[i], PRS_ackley_avg[i], PRS_rastrigin_avg[i])
+   }
+  
+  df <- data.frame(
+    Method = rep(rep(c("MS", "PRS"), each = 2), length(dimensions)),
+    Dimension = rep(dimensions, each = 4),
+    Value = values
+  )
+  
+  plot_point <- ggplot(df, aes(x = as.factor(Dimension), y = Value, color = Method)) +
+    geom_point(position = position_dodge(width = 0.8), size = 3) +
+    labs(x = "Dimension", y = "Optimization Value", title = "Comparison of MS and PRS") +
+    theme_minimal()
 
+  print(plot_point)
+
+  plot_boxplot <- ggplot(df, aes(x = as.factor(Dimension), y = Value, color = Method)) +
+    geom_boxplot(position = position_dodge(width = 0.8), width = 0.7, alpha = 0.7) +
+    labs(x = "Dimension", y = "Optimization Value", title = "Comparison of MS and PRS") +
+    theme_minimal()
+
+  print(plot_boxplot)
 }
 
 main()
